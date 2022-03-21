68533317c446f6f66a65cb3774e20ecfd6ce974b
MaksimMarynau <oliverknightbel@gmail.com>
2022-03-21 20:54:09 +0100

    setup docker-compose, updated settings and requirements

--- Backend/rr_project/rr_project/settings.py
+++ Backend/rr_project/rr_project/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env.dev'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG'))
+DEBUG = int(env('DEBUG'))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS'.split(' '))
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
