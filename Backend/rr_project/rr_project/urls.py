1b7db604968611f01706e988a288a857d4f079ac
MaksimMarynau <oliverknightbel@gmail.com>
2022-03-28 21:25:53 +0200

    add list api in url

--- Backend/rr_project/rr_project/urls.py
+++ Backend/rr_project/rr_project/urls.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+api_urls = [
+    path('api/recipe/', include('recipe.urls'), name='recipe-api'),
+]
+
+
+class ListApi(APIView):
+
+    def get(self, request, format=None):
+        api_urls = {
+            'List-recipes': 'recipe/list/',
+            'Detail-recipes': 'recipe/<int:id>/',
+            'Create-recipe': 'recipe/create/',
+        }
+
+        return Response(api_urls)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/recipe/', include('recipe.urls')),
+    path('api/list/', ListApi.as_view()),
+    path('', include(api_urls)),
 ]
