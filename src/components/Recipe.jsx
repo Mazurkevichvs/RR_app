c3f4b4d0db198ce546b37edae8e7fab0425d9067
Mazurkevichvs <mazurkevichvs@gmail.com>
2022-04-07 23:58:51 +0200

    modify recipe page

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import './Recipe.scss';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowUpLong} from '@fortawesome/free-solid-svg-icons'
+import {faArrowUpLong} from '@fortawesome/free-solid-svg-icons';
+import './Recipe.scss';
 
 function Recipe() {
   return (
     <footer>
         <section className='recipe__top'>
             
-        <div className='arrow'><FontAwesomeIcon icon={faArrowUpLong} /></div>
-        <p className='username'>by User123</p>
+          <div className='arrow'><FontAwesomeIcon icon={faArrowUpLong} /></div>
+          <p className='username'>by User123</p>
         </section>
-        <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </ul>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, minus officia? Necessitatibus exercitationem esse ea, sapiente et nostrum vel velit fugiat non, praesentium, aperiam consequuntur voluptates sunt aspernatur at! Excepturi deleniti ea sed quaerat non possimus suscipit tempora, quae, quasi blanditiis quia vel. Quos veniam harum eligendi illo minus necessitatibus laudantium ipsa quod voluptatem? Suscipit voluptates aspernatur culpa, vel minus provident, enim, id harum ipsa facilis alias blanditiis voluptatum praesentium hic. Odit in veniam blanditiis voluptatibus, dignissimos aut nemo! Omnis recusandae corporis ipsum minima ab distinctio aliquid a, numquam inventore. Possimus assumenda modi nemo quidem, dolore temporibus dolor quo sed incidunt id eius, earum voluptate doloribus voluptas corrupti ab impedit. Repudiandae, animi repellat quo exercitationem officiis rem similique rerum provident, molestias facere omnis sunt architecto. Possimus labore voluptatem quisquam mollitia voluptas sint facilis vitae, ut voluptate quo eos adipisci soluta omnis atque sunt! Maiores saepe, eligendi voluptates vero minus optio cupiditate ad aut rem repellat dolorem corrupti velit a quos pariatur distinctio nam. Numquam laudantium nobis fugit dignissimos id, asperiores hic sequi est minus porro consequuntur, minima doloremque similique esse earum eum necessitatibus natus cumque. Possimus, animi? Reiciendis distinctio ducimus vel sequi in ipsum blanditiis autem fugit eveniet, modi nulla?</p>
+        <section className='recipe__description'>
+          <ul className='ingredients'>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+              <li>ingredient</li>
+          </ul>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam aperiam accusamus ducimus asperiores neque nihil illo veritatis voluptate itaque? Officiis esse voluptatibus reprehenderit ex doloribus cumque beatae consectetur, modi quae quaerat pariatur dolorem explicabo saepe dolores consequatur ducimus illo iure quo blanditiis eum, voluptatem similique natus. Dicta quisquam fugit a ducimus amet ipsa, voluptatum fugiat quaerat reiciendis praesentium ipsam rem perspiciatis deserunt sed quis voluptatibus. Aspernatur accusantium recusandae dolorum eum voluptas autem pariatur quia repellat?</p>
+        </section>
+        
     </footer>
   )
 }
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Button from '../components/button';
 import {Link} from 'react-router-dom';
 
 function LogIn() {
+  const [login, setLogin] = useState('');
+
+  const onChangeValue = (event) => {
+    setLogin(() => (event.target.value));
+    console.log(login);
+  }
+
   return (
     <div className='container'>
-      <input className='login__input' type="text" placeholder='example@mail.com '/>
+      <input onChange={onChangeValue} className='login__input' type="text" placeholder='example@mail.com '/>
       <Link to="/Generator"><Button className={"btn__logs"} name={"log-in with Email"}/></Link>
-      <Button className={"btn__logs"} name={"Continue offline"}/>
+      <Link to="/Generator"><Button className={"btn__logs"} name={"Continue offline"}/></Link>
     </div>
   )
 }
