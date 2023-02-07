6c35b46e2608dc311dffc158f3b284963821edb8
Mazurkevichvs <mazurkevichvs@gmail.com>
2023-02-07 22:09:50 +0100

    change recipe styles, usenavigate hook used

--- Frontend/src/components/RecipeList.jsx
+++ Frontend/src/components/RecipeList.jsx
@@ -10,7 +10,6 @@ const recipe = recipes.map(rec => <RecipeItem key={rec.id} recipe={rec}/>)
   return (
     <div className="recipe__wrapper">
       <div className='recipe__list'>
-        
         <div className='recipe__item first__recipe'>
             +
         </div>
--- Frontend/src/pages/LogIn.jsx
+++ Frontend/src/pages/LogIn.jsx
@@ -1,55 +1,47 @@
-import { React, useState } from "react";
-import {Input, Button} from '../components';
-import { Link } from "react-router-dom";
-import {useDispatch} from 'react-redux';
-import {setLogin, setPassword} from '../redux/slices/loginSlice';
-import axios from "axios";
+import { React, useState } from 'react';
+import { Input, Button } from '../components';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setLogin, setPassword } from '../redux/slices/loginSlice';
+import axios from 'axios';
 
 function LogIn() {
-
   const dispatch = useDispatch();
-
   const [loginInput, setLoginInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  const navigate = useNavigate();
 
   const logIn = async (loginInput, passwordInput) => {
     const user = {
       username: loginInput,
-      password: passwordInput
-    }
-    
-    await axios.post('http://localhost:8000/api/account/login/', user)
-    .then(res => {
-      dispatch(setLogin(loginInput));  
-    dispatch(setPassword(passwordInput));
-    console.log(res)
-    })
-    .catch(err => console.log("ERROR",err))
+      password: passwordInput,
+    };
+    await axios
+      .post('http://localhost:8000/api/account/login/', user)
+      .then((res) => {
+        dispatch(setLogin(loginInput));
+        dispatch(setPassword(passwordInput));
+        console.log(res);
+        navigate('/Generator');
+      })
+      .catch((err) => console.log('ERROR', err));
+  };
 
-    
-};
-  
   return (
-    <div className="container"> 
-      <Input placeholder = 'login' setInputValue={setLoginInput}/>
-      <Input placeholder = 'password' setInputValue={setPasswordInput}/>
-      <p>Don't have an account? <Link to="/Registration">Register now!</Link></p>
-      {loginInput && passwordInput ? (
-        <Link to="/Generator">
-          <Button
-            onClick={() => logIn(loginInput, passwordInput)}
-            className={"btn__logs"}
-            name={"Log in"}
-          />
-        </Link>
-      ) : (
-        <Button
-          className={"btn__logs btn__disabled"}
-          name={"Log in"}
-        />
-      )}
+    <div className="container">
+      <Input placeholder="login" setInputValue={setLoginInput} />
+      <Input placeholder="password" setInputValue={setPasswordInput} />
+      <p>
+        Don't have an account? <Link to="/Registration">Register now!</Link>
+      </p>
+      <Button
+        onClick={() => logIn(loginInput, passwordInput)}
+        className={loginInput && passwordInput ? 'btn__logs' : 'btn__logs btn__disabled'}
+        name={'Log in'}
+      />
       <Link to="/Generator">
-        <Button className={"btn__logs"} name={"Continue offline"} />
+        <Button className={'btn__logs'} name={'Continue offline'} />
       </Link>
     </div>
   );
